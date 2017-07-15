3b3088ca04e324d943e584c4917d3c7c3807c00f
srujan <asrujansai@gmail.com>
2017-07-15 08:33:58 +0530

    completed testing required2

--- app/src/main/java/com/example/srujansai/myapplication/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/srujansai/myapplication/MyFirebaseMessagingService.java
@@ -4,16 +4,47 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by srujan sai on 10-07-2017.
  */
 
-public class MyFirebaseMessagingService extends Service {
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-        return null;
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+           //     // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+             //   scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+              //  handleNow();
+            }//
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // m
     }
 }
